abe5a5b378e7ba44449d57d0d3b6153b748f1d4d
Munia Akter <muniaakter@munias-mbp.lan>
2019-05-30 16:32:30 -0400

    j

---
+++ UtilityClass.java
@@ -0,0 +1,17 @@
+package CapitalOne;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class UtilityClass {
+	public Properties readpropertyfile () {
+		FileInputStream fs = new FileInputStream(null);
+		Properties prop = new Properties ();
+		prop.load(fs);
+		return prop;
+		
+		
+		//hueahjkrwef,sdncej
+	}
+//hhhh
+}
